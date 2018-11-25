53c32e1d086a754105312d1148bb15aadc1c6207
marcisvil <44918188+marcisvil@users.noreply.github.com>
2018-11-25 10:57:12 +0200

    Conflict search

--- src/com/vilskersts/data/arrayMoving.java
+++ src/com/vilskersts/data/arrayMoving.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class arrayMoving {
+    public arrayMoving() {
+    }
+
     public static void main(String[] args) {
 
         int[] array = {1,2,3,4,5,6};
+        int[] array2 = {1,2,3,4,5,7};
+
 
 
         System.out.println(Arrays.toString(array));
