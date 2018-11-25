08168c9123664563051f70f6608141f887cf1663
marcisvil <44918188+marcisvil@users.noreply.github.com>
2018-11-25 10:48:11 +0200

    Conflict search

---
+++ src/com/vilskersts/data/arrayMoving.java
@@ -0,0 +1,20 @@
+package com.vilskersts.data;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class arrayMoving {
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6};
+
+
+        System.out.println(Arrays.toString(array));
+
+//        for (int i = 0; i < array.length-1 ; i++) {
+//
+//
+//        }
+
+    }
+}
